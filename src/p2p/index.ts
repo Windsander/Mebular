9895ba5d934580654bbf19086cb44e6077876b55
Windsander <Windsander@users.noreply.github.com>
2024-10-15 21:09:57 +0800

    feat(p2p): 擴展 模組導出面

--- src/p2p/index.ts
+++ src/p2p/index.ts
@@ -1,8 +1,59 @@
 // P2P 模块入口
 
-export { P2PNode, type P2PConfig, type P2PNetwork, type Connection, type ConnectionState, type PeerId, type PeerInfo, type P2PNodeOptions } from './P2PNetwork.js';
-export { DeviceDiscovery, type DiscoveryOptions } from './DeviceDiscovery.js';
+export {
+  P2PNode,
+  type P2PConfig,
+  type P2PNetwork,
+  type Connection,
+  type ConnectionState,
+  type PeerId,
+  type PeerInfo,
+  type P2PNodeOptions,
+  type P2PNodeIdentity,
+} from './P2PNetwork.js';
+export {
+  DeviceDiscovery,
+  type DiscoveryOptions,
+  type DeviceDiscoveryOptions,
+  type BonjourService,
+  type BonjourServiceInstance,
+  type BonjourServiceFactory,
+} from './DeviceDiscovery.js';
 export { ConnectionManager, type ConnectionManagerOptions } from './connection/ConnectionManager.js';
-export { AuthenticationHandshake, type AuthHandshakeOptions, type AuthRequest, type AuthResponse, type AuthSession } from './handshake/AuthenticationHandshake.js';
-export { NATTraversal, type NATTraversalOptions, type NATType, type RelayServer } from './nat/NATTraversal.js';
-export { SecureChannelImpl, type SecureChannelOptions, type SecureChannel } from './secure/SecureChannelImpl.js';
+export {
+  AuthenticationHandshake,
+  canonicalCertificateData,
+  bytesToBase64,
+  base64ToBytes,
+  bytesToHex,
+  hexToBytes,
+  type AuthHandshakeOptions,
+  type AuthRequest,
+  type AuthResponse,
+  type AuthSession,
+  type DeviceCertificate,
+  type LocalIdentity,
+} from './handshake/AuthenticationHandshake.js';
+export {
+  NATTraversal,
+  isPublicIPv4,
+  type NATTraversalOptions,
+  type NATType,
+  type RelayServer,
+  type NatProber,
+  type HolePunchChannel,
+} from './nat/NATTraversal.js';
+export {
+  SecureChannelImpl,
+  type SecureChannelOptions,
+  type SecureChannel,
+} from './secure/SecureChannelImpl.js';
+export {
+  InMemoryHub,
+  InMemoryConnection,
+  MessageQueue,
+  type ConnectionProvider,
+  type MutableAuthenticationConnection,
+  type ActivityTrackingConnection,
+  type PingCapableConnection,
+} from './transport/InMemoryTransport.js';
